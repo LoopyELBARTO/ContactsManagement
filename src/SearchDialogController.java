b6b7d583628371090179e91173b62f93fa6d30b7
LoopyELBARTO <loopyelbarto@gmail.com>
2017-12-13 11:48:44 -0500

    Started working on BST

--- src/SearchDialogController.java
+++ src/SearchDialogController.java
@@ -1,2 +1,14 @@
+import ch07.trees.BSTInterface;
+import ch07.trees.BinarySearchTree;
+
 public class SearchDialogController {
+    BinarySearchTree<String> bstBusiness = new BinarySearchTree<>();
+    Iterable<Business> iter;
+
+    private void loadDataToBST(){
+        iter = bstBusiness.getIterator(BSTInterface.Traversal.Inorder);
+        while (iter.)
+        bstBusiness.add(new Business().getBusinessName());
+    }
+
 }
